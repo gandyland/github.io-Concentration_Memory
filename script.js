5ff11d2c9e8b79f1a3396627bd9a11b2f7c8383e
Andrew Young <prince.of.utah@gmail.com>
2016-06-08 16:01:47 -0400

    tried to add timer and deleted it and changed syntax

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var concnetration_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
 var concnetration_attributes = [];
-var memory_tile_ids = [];
+var concentration_tile_ids = [];
 var tiles_flipped = 0;
-Array.prototype.memory_tile_shuffle = function(){
+Array.prototype.concentration_tile_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
         j = Math.floor(Math.random() * (i+1));
@@ -14,41 +14,41 @@ Array.prototype.memory_tile_shuffle = function(){
 function newBoard(){
 	tiles_flipped = 0;
 	var output = '';
-    concnetration_array.memory_tile_shuffle();
+    concnetration_array.concentration_tile_shuffle();
 	for(var i = 0; i < concnetration_array.length; i++){
-		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+concnetration_array[i]+'\')"></div>';
+		output += '<div id="tile_'+i+'" onclick="concentrationFlipTile(this,\''+concnetration_array[i]+'\')"></div>';
 	}
-	document.getElementById('memory_board').innerHTML = output;
+	document.getElementById('concentration_board').innerHTML = output;
 }
-function memoryFlipTile(tile,val){
+function concentrationFlipTile(tile,val){
 	if(tile.innerHTML == "" && concnetration_attributes.length < 2){
-		tile.style.background = '#CCCCCC';
+		tile.style.background = '#000';
 		tile.innerHTML =  '<img src="' + val + '.jpg"/>';
 		if(concnetration_attributes.length == 0){
 			concnetration_attributes.push(val);
-			memory_tile_ids.push(tile.id);
+			concentration_tile_ids.push(tile.id);
 		} else if(concnetration_attributes.length == 1){
 			concnetration_attributes.push(val);
-			memory_tile_ids.push(tile.id);
+			concentration_tile_ids.push(tile.id);
 			if(concnetration_attributes[0] == concnetration_attributes[1]){
 				tiles_flipped += 2;
 				concnetration_attributes = [];
-            	memory_tile_ids = [];
+            	concentration_tile_ids = [];
 				if(tiles_flipped == concnetration_array.length){
 					alert("Congratulations! You Win! Generating new board.");
-					document.getElementById('memory_board').innerHTML = "";
+					document.getElementById('concentration_board').innerHTML = "";
 					newBoard();
 				}
 			} else {
 				function flip2Back(){
-				    var tile_1 = document.getElementById(memory_tile_ids[0]);
-				    var tile_2 = document.getElementById(memory_tile_ids[1]);
+				    var tile_1 = document.getElementById(concentration_tile_ids[0]);
+				    var tile_2 = document.getElementById(concentration_tile_ids[1]);
 				    tile_1.style.background = 'url(card_stock.jpg) no-repeat';
             	    tile_1.innerHTML = "";
 				    tile_2.style.background = 'url(card_stock.jpg) no-repeat';
             	    tile_2.innerHTML = "";
 				    concnetration_attributes = [];
-            	    memory_tile_ids = [];
+            	    concentration_tile_ids = [];
 				}
 				setTimeout(flip2Back, 700);
 			}
